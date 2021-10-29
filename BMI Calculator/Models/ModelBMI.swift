bc771c1f72c98df31ff1d054bb0bfe1ecd2d833c
AriNair44 <93320198+AriNair44@users.noreply.github.com>
2021-10-29 03:20:49 -0400

    Add files via upload

---
+++ BMI Calculator/Models/ModelBMI.swift
@@ -0,0 +1,15 @@
+//
+//  ModelBMI.swift
+//  BMI Calculator
+//
+//  Created by Ari Nair on 6/22/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct ModelBMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
